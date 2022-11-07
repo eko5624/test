60f2ee5eca820763e7efbcf804114eec452c697c
eko5624 <562433403@qq.com>
2022-11-08 00:23:57 +0800

    Create release-dev.sh

---
+++ release-dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+github_repository="eko5624/test"
+
+# Delete assets
+asset_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${github_repository}/releases/tags/dev \
+  | jq -r '.assets[] | select(.name | startswith("$1")) | .id') 
+  
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -X DELETE \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${github_repository}/releases/assets/$asset_id    
+
+# Release assets
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${github_repository}/releases \
+  -d '{"tag_name": "dev"}'
+  
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${github_repository}/releases/tags/dev | jq -r '.id')
+  
+for f in $2/*.zst; do 
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+    -X POST -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: $(file -b --mime-type $f)" \
+    https://uploads.github.com/repos/${github_repository}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
+done
