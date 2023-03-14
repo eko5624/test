010120682c74017cacdafbac1c288c075fea628f
eko5624 <562433403@qq.com>
2023-03-14 23:30:18 +0800

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -18,7 +18,7 @@ for dylib in "${mpv_otool[@]}"; do
   mpv_dylib_otool=($(otool -L $dylib | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
   mpv_dylibs_otool+=("${mpv_dylib_otool[@]}")
 done
-mpv_dylibs_otool=($(echo "${mpv_dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#mpv_dylibs_otool=($(echo "${mpv_dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 echo "${mpv_dylibs_otool[@]}" > $PACKAGES/mpv/build/mpv_dylibs_otool
 
 dylibs_otool=()
@@ -26,7 +26,7 @@ for dylib in "${mpv_dylibs_otool[@]}"; do
   dylib_otool=($(otool -L $dylib | grep -e '\t' | grep -Ev "\/usr\/lib|\/System|@rpath" | awk '{ print $1 }'))
   dylibs_otool+=("${dylib_otool[@]}")
 done	
-dylibs_otool=($(echo "${dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+#dylibs_otool=($(echo "${dylibs_otool[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 echo "${dylibs_otool[@]}" > $PACKAGES/mpv/build/dylibs_otool
 
 all_dylibs=(${mpv_otool[@]} ${mpv_dylibs_otool[@]} ${dylibs_otool[@]})
