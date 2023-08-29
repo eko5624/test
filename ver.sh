40dd64c48f380d91e4afc730c3441116413ad352
eko5624 <562433403@qq.com>
2023-08-29 19:13:43 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -11,7 +11,7 @@ curl -OL https://github.com/eko5624/nginx-nosni/raw/master/old.json
 declare -A dict
 while IFS="=" read -r key value; do
     dict[$key]=$value
-done << (jq -r 'to_entries | map("\(.key)=\(.value | tostring)") | .[]' old.json)
+done << (jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]' old.json)
 
 # 打印关联数组
 for key in "${!dict[@]}"; do
