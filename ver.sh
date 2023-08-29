15cfbefdbd7722674e2c07ca139f794d52d6c145
eko5624 <562433403@qq.com>
2023-08-29 16:17:56 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-json_data=$(curl -s "https://github.com/eko5624/nginx-nosni/raw/master/old.json")
+curl -o https://github.com/eko5624/nginx-nosni/raw/master/old.json
 # 定义关联数组
 declare -A data
 
 # 将 JSON 内容解析为关联数组
-eval $(echo data=$(echo $json_data | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | join(" ")'))
+eval $(echo data=$(cat old.json | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | join(" ")'))
 
 # 打印关联数组
 for key in "${!data[@]}"; do
