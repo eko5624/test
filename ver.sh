531771521d9a88a11a5bf10bcf1b8ed920bc31e8
eko5624 <562433403@qq.com>
2023-08-31 19:45:38 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# 定义关联数组
-declare -A assoc_array
+json_ver=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master/old.json")
+declare -A ver_array
+while IFS="=" read -r key value; do
+    ver_array[$key]=$value
+done < <(echo "$json_ver" | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]')
 
-# 读取JSON字典内容
-json_data='{"key1": "value1", "key2": "value2", "key3": "value3"}'
-
-# 使用jq工具解析JSON数据
-parsed_json=$(echo "$json_data" | jq -r 'to_entries | map("\(.key)=\(.value)") | join(",")')
-
-# 将解析后的JSON数据存储到关联数组中
-eval "assoc_array=($parsed_json)"
-
-# 打印关联数组的内容
-echo "关联数组的内容："
-for key in "${!assoc_array[@]}"; do
-    echo "$key => ${assoc_array[$key]}"
-done
+VER_BINUTILS=${ver_array[binutils]}
+VER_GCC=${ver_array[GCC]}
+echo $VER_BINUTILS $VER_GCC
