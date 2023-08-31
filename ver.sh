07779514809e5c9c6c0e4f24c7a664e792de504b
eko5624 <562433403@qq.com>
2023-08-31 19:15:56 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
 json_ver=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master/old.json")
-declare -A dict
+declare -A ver_array
 while IFS="=" read -r key value; do
-    dict[$key]=$value
+  ver_array[$key]=$value
 done < <(echo "$json_ver" | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]')
-
-# 示例
-echo "${dict[mpv]}"
-
+for key in "${!ver_array[@]}"; do
+  echo "$key => ${ver_array[$key]}"
+done
