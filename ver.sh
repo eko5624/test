1639257fc39d2d1205e766053b38605331ac38be
eko5624 <562433403@qq.com>
2023-08-31 23:00:20 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-json_ver=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master/old.json")
 declare -A ver_array
-while IFS="=" read -r key value; do
-    ver_array[$key]=$value
-done < <(echo "$json_ver" | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]')
+
+json_file=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master/old.json")
+keys_content=$(jq -r '. | keys | .[]' "$json_file")
+while read -r key; do
+  value=$(jq -r ".[$key]" "$json_file")
+  ver_array[$key]=$value
+done <<< "$keys_content"
 
 VER_BINUTILS=${ver_array[binutils]}
 VER_GCC=${ver_array[GCC]}
