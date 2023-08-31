e4e4013b74a8d43db105ab75e7313404fd0a4665
eko5624 <562433403@qq.com>
2023-08-31 10:33:55 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -4,7 +4,7 @@ json_ver=$(curl -s "https://raw.githubusercontent.com/eko5624/nginx-nosni/master
 declare -A dict
 while IFS="=" read -r key value; do
     dict[$key]=$value
-done < <(jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]' echo "$json_ver")
+done < <(echo "$json_ver" | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]')
 
 # 示例
 echo "${dict[mpv]}"
